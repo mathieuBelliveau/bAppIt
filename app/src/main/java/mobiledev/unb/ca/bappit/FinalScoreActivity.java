73ad85dd34cede3d339700896b959c67cb7b7d56
Suzannah Smith <ssmith13@id415m24.cs.unb.ca>
2018-02-10 16:31:42 -0400

    added navigation and gesture recognition

--- app/src/main/java/mobiledev/unb/ca/bappit/FinalScoreActivity.java
+++ app/src/main/java/mobiledev/unb/ca/bappit/FinalScoreActivity.java
@@ -1,13 +1,60 @@
 package mobiledev.unb.ca.bappit;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class FinalScoreActivity extends AppCompatActivity {
+
+    public static final String FINAL_SCORE = "score";
+
+    private TextView displayScoreText;
+    private Button replayBtn;
+    private Button mainMenuBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_score);
+
+        int finalScore = getIntent().getIntExtra(FINAL_SCORE, 0);
+//        Log.i("score", finalScore + "");
+
+        displayScoreText = (TextView) findViewById(R.id.score_display_txt);
+        displayScoreText.setText(String.valueOf(finalScore));
+
+        replayBtn = (Button) findViewById(R.id.replay_btn);
+        replayBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                replayGame();
+            }
+        });
+
+        mainMenuBtn = (Button) findViewById(R.id.main_menu_btn);
+        mainMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FinalScoreActivity.this, StartMenuActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void replayGame() {
+        Intent intent = new Intent(FinalScoreActivity.this, GameActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/mobiledev/unb/ca/bappit/GameActivity.java
+++ app/src/main/java/mobiledev/unb/ca/bappit/GameActivity.java
@@ -1,12 +1,30 @@
 package mobiledev.unb.ca.bappit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GameActivity extends Activity {
+public class GameActivity extends AppCompatActivity {
+
+    private static final String DEBUG_TAG = "debug";
 
     private View gameView;
+    private int score;
+
+    private Button gameButton;
+    private Button finishButton;
+    private TextView scoreText;
+
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +32,70 @@ public class GameActivity extends Activity {
         setContentView(R.layout.activity_game);
         gameView = getWindow().getDecorView();
         hideSystemUI();
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
+        score = 0;
+
+        gameButton = (Button) (findViewById(R.id.game_btn));
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                increaseScore();
+            }
+        });
+
+        finishButton = (Button) (findViewById(R.id.finish_btn));
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gameOver();
+            }
+        });
+
+        scoreText = (TextView) findViewById(R.id.score_txt);
+
+    }
+
+    private void increaseScore() {
+        score++;
+        scoreText.setText("Score: " + score);
+    }
+
+    private void gameOver() {
+        Intent finishIntent = new Intent(GameActivity.this, FinalScoreActivity.class);
+        finishIntent.putExtra(FinalScoreActivity.FINAL_SCORE, score);
+        startActivity(finishIntent);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG,"onDown: " + event.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            Log.d(DEBUG_TAG, "onTap: " + e.toString());
+            return true;
+        }
     }
 
     private void hideSystemUI() {
