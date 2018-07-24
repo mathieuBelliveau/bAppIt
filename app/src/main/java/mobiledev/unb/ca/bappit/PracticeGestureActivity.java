54f2f5890b656f14a2cf5f943316bac05c6bfe5a
Mathieu - Mobile <magnumit.ca@gmail.com>
2018-07-24 07:28:19 -0300

    Practice Gesture Work

--- app/src/main/java/mobiledev/unb/ca/bappit/PracticeGestureActivity.java
+++ app/src/main/java/mobiledev/unb/ca/bappit/PracticeGestureActivity.java
@@ -1,9 +1,11 @@
 package mobiledev.unb.ca.bappit;
 
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -21,6 +23,11 @@ public class PracticeGestureActivity extends GestureCompatActivity{
     public final static String GESTURE_CODE = "gestureCode";
 
     private ImageView practiceGestureImage;
+    private VideoView practiceGestureVideo;
+
+    private int gestureID;
+    private String gestureName;
+    private int gestureCode;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -29,25 +36,45 @@ public class PracticeGestureActivity extends GestureCompatActivity{
         setContentView(R.layout.activity_practice_gesture);
 
         Intent intent = getIntent();
-        int gestureID = intent.getIntExtra(GESTURE_ID, 0);
-        String gestureName = intent.getStringExtra(GESTURE_NAME);
-        int gestureCode = intent.getIntExtra(GESTURE_CODE, 0);
+        gestureID = intent.getIntExtra(GESTURE_ID, 0);
+        gestureName = intent.getStringExtra(GESTURE_NAME);
+        gestureCode = intent.getIntExtra(GESTURE_CODE, 0);
 
         TextView gestureNameView = (TextView) findViewById(R.id.practice_action_name);
         gestureNameView.setText(gestureName);
 
-        VideoView video = (VideoView)findViewById(R.id.practice_tutorial_video);
+        practiceGestureImage = (ImageView) findViewById(R.id.practice_gesture_img);
+        practiceGestureImage.setVisibility(View.INVISIBLE);
+
+        practiceGestureVideo = (VideoView)findViewById(R.id.practice_tutorial_video);
+        practiceGestureVideo.setVisibility(View.VISIBLE);
         Uri uri = Uri.parse("android.resource://" + getPackageName()+ "/" + gestureID);
-        video.setVideoURI(uri);
-        video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+        practiceGestureVideo.setVideoURI(uri);
+        practiceGestureVideo.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mp) {
-                mp.setLooping(true);
+                mp.setLooping(false);
                 mp.setVolume(0, 0);
             }
         });
-        video.start();
-        //TODO - Disappear video and start practice
+        practiceGestureVideo.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                mp.setVolume(100,100);//FIXME - sharedprefs instead
+                mp.stop();//FIXME - unsure if proper to stop video here.
+                PracticeGestureActivity context = (PracticeGestureActivity) getApplicationContext();
+                context.beginPractice();
+                //TODO - Play after the listener is pressed and the video completes
+            }
+        });
+        practiceGestureVideo.start();
+    }
+
+    public void beginPractice(){
+//        practiceGestureVideo.stopPlayback();//FIXME - stop here or at 55?
+        practiceGestureVideo.setVisibility(View.INVISIBLE);
+        practiceGestureImage.setVisibility(View.VISIBLE);
+
+        setCurrentGesture(Gesture.values()[gestureCode]);
     }
 
     public void gestureMatch(Gesture gesture)
